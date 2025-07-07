e42166bb4b2218b952892bb6e16ac21517ffd474
tonstey <tonyvuong245@gmail.com>
2025-07-07 01:56:25 -0700

    Edit 1: Replaced Router and editted url

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import { store } from './redux/store.js'
 import App from './App.jsx'
@@ -8,9 +7,9 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </Provider>
  
 )
